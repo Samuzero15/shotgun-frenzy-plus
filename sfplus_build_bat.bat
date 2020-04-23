0ffa54c38ce841031070dab7ac0b34a2d80987c2
Samuzero15 <samueloml@gmail.com>
2020-04-23 01:04:54 -0400

    Batch file finished, and new builds relased.

--- sfplus_build_bat.bat
+++ sfplus_build_bat.bat
@@ -34,25 +34,31 @@ chgcolor 0B
 
 echo Pick a number for your option.
 chgcolor 09
+
 echoj 1) 
-echo  Build with the local build.
+echo  Build a dev version with the local build.
 
 echoj 2) 
-echo  Build with the git build.
+echo  Build a relase with the local build.
 
 echoj 3) 
+echo  Build with the git build.
+
+echoj 4) 
 echo  Quit.
 
 CHOICE /C 1234567 /N /M "Choose Option (Number Keys):"
-IF ERRORLEVEL 3 GOTO LEAVE
-IF ERRORLEVEL 2 GOTO GIT_BUILD
-IF ERRORLEVEL 1 GOTO LOC_BUILD
 
+IF ERRORLEVEL 4 GOTO LEAVE
+IF ERRORLEVEL 3 GOTO GIT_BUILD
+IF ERRORLEVEL 2 GOTO REL_BUILD
+IF ERRORLEVEL 1 GOTO DEV_BUILD
 
 
 :GIT_BUILD
 chgcolor 0B
-echo Compiling Shotgun Frenzy Plus Rev#: %REVISIONNUMBER% (Full Compression)...
+echo Compiling Shotgun Frenzy Plus Rev#: %REVISIONNUMBER%...
+pause
 del .\builds\sfplus_r%REVISIONNUMBER%.pk3 /q
 
 cd pk3
@@ -60,12 +66,31 @@ cd pk3
 pause
 goto MENU
 
-:LOC_BUILD
-echo Compiling Shotgun Frenzy Plus Local Dev Build...
-del .\builds\sfplus.pk3 /q
+:REL_BUILD
+chgcolor 0B
+cd pk3
+	for /f "tokens=5 delims=:, " %%A in (GAMEINFO.txt) do set RELASE=%%A
+
+echo Compiling Shotgun Frenzy Plus ( %RELASE% )...
+pause
+del .\builds\sfplus_%RELASE%.pk3 /q
 
 cd pk3
-7za a -y -tzip -mx=0 -mmt -x!.svn ..\builds\sfplus.pk3 .\
+7za a -y -tzip -mx=9 -mmt -x!.svn ..\builds\sfplus_%RELASE%.pk3 .\
+
+pause
+goto MENU
+
+:DEV_BUILD
+chgcolor 0B
+
+echo Compiling Shotgun Frenzy Plus Local Dev Build...
+pause
+del .\builds\sfplus-d.pk3 /q
+
+cd pk3
+7za a -y -tzip -mx=9 -mmt -x!.svn ..\builds\sfplus-d.pk3 .\
+
 pause
 goto MENU
 
