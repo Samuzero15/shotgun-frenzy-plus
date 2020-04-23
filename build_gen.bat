3825f461f5d59f034113186dbcfc409f5a719b2b
Samuzero15 <samueloml@gmail.com>
2020-04-23 02:42:05 -0400

    Batch routing fixed for more organized relases, and also another git relase

--- build_gen.bat
+++ build_gen.bat
@@ -9,46 +9,48 @@ echo --------------------------------------------
 
 for /f "delims=" %%i in ('git rev-list HEAD --count') do set REVISIONNUMBER=%%i
 
+cd pk3
+	for /f "tokens=5 delims=:, " %%A in (GAMEINFO.txt) do set RELASE=%%A
+
 :MENU
 cd /d %~dp0
 cls
 
 chgcolor 1B
 
-echoj $C9 ================================== $bb
+echoj $C9 $CD $CD $CD $CD $CD $CD $CD $CD $cd $CD $CD $CD $CD $CD $CD $CD $CD $CD $cd $CD $CD $CD $CD $CD $CD $CD $CD $CD $cd $CD $CD $CD $CD $CD $bb
 echoj $0a $ba " Shotgun Frenzy Plus Pk3 builder! " $ba 
-echoj $0a $C8 ================================== $bc
-echo.
+echoj $0a $C8 $CD $CD $CD $CD $CD $CD $CD $CD $cd $CD $CD $CD $CD $CD $CD $CD $CD $CD $cd $CD $CD $CD $CD $CD $CD $CD $CD $CD $cd $CD $CD $CD $CD $CD $bc
 echo.
 echo.
 
-chgcolor 01
+chgcolor 0E
 echoj "Path: "
 echo %~dp0
 echo.
 echoj "Git Revision: "
 echo %REVISIONNUMBER%
 echo.
+echoj "Relase tentative: "
+echo %Relase%
+echo.
 
+chgcolor 1B
+echoj $C9 $CD $CD $CD $CD $CD $CD $CD $CD $cd $CD $CD $CD $CD $CD $CD $CD $CD $CD $cd $CD $CD $CD $CD $CD $CD $CD $CD $CD $cd $CD $CD $CD $CD $CD $bb
+echoj $0a $ba " Pick a number for your option.   " $ba
+
+echoj $0a $ba "1) Build the dev version.         " $ba
+
+echoj $0a $ba "2) Build the relase version.      " $ba
+
+echoj $0a $ba "3) Build the git version.         " $ba
+
+echoj $0a $ba "4) Quit.                          " $ba                  
+
+echoj $0a $C8 $CD $CD $CD $CD $CD $CD $CD $CD $cd $CD $CD $CD $CD $CD $CD $CD $CD $CD $cd $CD $CD $CD $CD $CD $CD $CD $CD $CD $cd $CD $CD $CD $CD $CD $bc
+echo.
 chgcolor 0B
-
-echo Pick a number for your option.
-chgcolor 09
-
-echoj 1) 
-echo  Build a dev version with the local build.
-
-echoj 2) 
-echo  Build a relase with the local build.
-
-echoj 3) 
-echo  Build with the git build.
-
-echoj 4) 
-echo  Quit.
-
 CHOICE /C 1234567 /N /M "Choose Option (Number Keys):"
-
 IF ERRORLEVEL 4 GOTO LEAVE
 IF ERRORLEVEL 3 GOTO GIT_BUILD
 IF ERRORLEVEL 2 GOTO REL_BUILD
@@ -59,24 +61,22 @@ IF ERRORLEVEL 1 GOTO DEV_BUILD
 chgcolor 0B
 echo Compiling Shotgun Frenzy Plus Rev#: %REVISIONNUMBER%...
 pause
-del .\builds\sfplus_r%REVISIONNUMBER%.pk3 /q
+del .\builds\gits\sfplus_r%REVISIONNUMBER%.pk3 /q
 
 cd pk3
-7za a -y -tzip -mx=9 -mmt -x!.svn ..\builds\sfplus_r%REVISIONNUMBER%.pk3 .\
+7za a -y -tzip -mx=9 -mmt -x!.svn ..\builds\gits\sfplus_r%REVISIONNUMBER%.pk3 .\
 pause
 goto MENU
 
 :REL_BUILD
 chgcolor 0B
-cd pk3
-	for /f "tokens=5 delims=:, " %%A in (GAMEINFO.txt) do set RELASE=%%A
 
 echo Compiling Shotgun Frenzy Plus ( %RELASE% )...
 pause
-del .\builds\sfplus_%RELASE%.pk3 /q
+del .\builds\relases\sfplus_%RELASE%.pk3 /q
 
 cd pk3
-7za a -y -tzip -mx=9 -mmt -x!.svn ..\builds\sfplus_%RELASE%.pk3 .\
+7za a -y -tzip -mx=9 -mmt -x!.svn ..\builds\relases\sfplus_%RELASE%.pk3 .\
 
 pause
 goto MENU
