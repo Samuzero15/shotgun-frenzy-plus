07970cce7681ee865df2827950253ee1df840903
Samuzero15 <samueloml@gmail.com>
2020-06-11 00:39:35 -0400

    Shell Grenades done, small fixes and improvements done too.

--- update.bat
+++ update.bat
@@ -2,16 +2,19 @@
 @echo off
 set PATH=%PATH%;%~dp0\bat_tools
 
-echo This bat will extract all files coming from sfplus_core-d.pk3
-echo and sfplus_res-d.pk3 if exist. This is intended to be used after
-echo each coding session, to update and commit directly to the git.
+echo This bat will extract all files coming from sfplus_core-d.pk3,
+echo sfplus_res-d.pk3 and sfplus_mus-d.pk3 if exist. This is intended 
+echo to be used after each coding session, to update and commit directly to the git.
+echo ________________________________________________________________________________
 echo You can also do it via Slade, but for lazy-ness sake, 
 echo I'd recommend you to use this way.
 chgcolor 0C
+echo ________________________________________________________________________________
 echo If you're working via Directories, beware! 
-echo This bat will delete the folders core and res from the file.
+echo This bat will delete the folders core, res and mus from the directory.
 echo To save your changes, build with the build bats and then try this file again.
 echo.
+echo ________________________________________________________________________________
 chgcolor 07
 echo If you dont need to execute this, close this window.
 pause
